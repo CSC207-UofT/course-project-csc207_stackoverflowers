e1dd6d5d445595f6400e79d18f3a4dcc50cc3d47
camillealve <camillealve2014@gmail.com>
2021-10-14 16:12:51 -0400

    updated GameMakerTest

--- Project/GameMakerTest.java
+++ Project/GameMakerTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+
 class GameMakerTest {
     GameMaker gm;
 
@@ -10,16 +11,14 @@ class GameMakerTest {
         GameMaker gm = new GameMaker();
     }
     
-    
     // Hi Camille, feel free to change this test as needed. - Maggie
     @Test(timeout = 50)
-    public void testFirstPrompt{
-    String expectedResult = FIRST_PROMPT_BEFORE_NAME + "\n"
-            + "Maggie" + "\n" 
-            + "FIRST_PROMPT_AFTER_NAME" + "\n"
-            + "Name: Farzana Rahman; age: 20; skills: teamwork (98)" + "\n"
-            + "Name: Maggie Huang; age: 20; skills: leadership (82)" + "\n"
-            + "Name: Mary Yijia Li; age: 19; skills: efficiency (99)";
-    assertTrue(gm.firstPrompt("Maggie"), expectedResult)
+    public void testFirstPrompt() {
+        String expectedResult = GamePrompts.FIRST_PROMPT_BEFORE_NAME + "Maggie" + "\n"
+            + GamePrompts.FIRST_PROMPT_AFTER_NAME
+            + "Name: Farzana Rahman; age: 20; skills: teamwork (98)\n"
+            + "Name: Maggie Huang; age: 20; skills: leadership (82)\n"
+            + "Name: Mary Yijia Li; age: 19; skills: efficiency (99)\n";
+        assertEquals(expectedResult, gm.firstPrompt("Maggie"));
     }
 }
