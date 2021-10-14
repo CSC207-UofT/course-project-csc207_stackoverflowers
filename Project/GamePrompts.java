0fe742b8ecd56610f5f4b59073bd75e18e9907b5
camillealve <camillealve2014@gmail.com>
2021-10-14 01:31:46 -0400

    updated GamePrompts and Intern

--- Project/GamePrompts.java
+++ Project/GamePrompts.java
@@ -1,12 +1,17 @@
 public class GamePrompts {
     /* this is the class for only game prompts, no method, similar to the java exception class in week 2 slides
-    // TODO:
+
     the prompt that asks player to enter name called ASK_FOR_NAME;
-    //TODO:
+
     the first display prompt called FIRST_PROMPT_BEFORE_NAME and FIRST_PROMPT_AFTER_NAME (See GameMaker.firstPrompt to
     see what content is in which):
     - "new manager at the company"
     -"here are the list of interns..."
     - format: name, age, skills
      */
+    public final String ASK_FOR_NAME = "Please enter your name:";
+
+    public final String FIRST_PROMPT_BEFORE_NAME = "Welcome! My friend, you have just been hired as a new manager.";
+
+    public final String FIRST_PROMPT_AFTER_NAME = "Here are your potential interns. Choose wisely ...";
 }
--- Project/Intern.java
+++ Project/Intern.java
@@ -3,27 +3,58 @@ import java.util.Map;
 
 
 public class Intern {
-    /*
-the constructor
-method get skills (private) -> returns the skills of the interns from a text file -> (create text file)
-we need to store the new interns to input into the HRSystem -> method generateinternfile
-- method getInternInfo
+    /* the constructor method get skills (private) -> returns the skills of the interns from a text file -> (create text file)
+    we need to store the new interns to input into the HRSystem -> method generateInternFile
+    - method getInternInfo
 
- */
-    // the attributes:
-    /* internName: the name of the intern
-    internAge: the age of the intern
-    internSkills: a map of skills where the key is the name of the skill and the value is the percentage.
+    */
+    private String internName;
+    private int internAge;
+    private HashMap<String, Integer> internSkills;
+
+    /**
+     * Construct an Intern, giving them the given internName,
+     * internAge, and internMoney.
+     *
+     * @param internName The name of this Intern
+     * @param internAge The age of this Intern
+     * @param internSkills A map of skills where the key is the name of the skill and the value is the percentage.
      */
-        private String internName;
-        private int internAge;
-        private HashMap<String, Integer> internSkills;
-
-        public Intern(String internName, Integer internAge, HashMap<String, Integer> skills) {
-            this.internName = internName;
-            this.internAge = internAge;
-            this.internSkills = skills;
-        }
-        //TODO: Add get method for intern name, age and skills.
-        //TODO:  Add a method internToString that returns the intern information as a string.
+    public Intern(String internName, int internAge, HashMap<String, Integer> internSkills) {
+        this.internName = internName;
+        this.internAge = internAge;
+        this.internSkills = internSkills;
     }
+
+    /**
+     * Return the name of this Intern
+     */
+    public String getInternName() { return this.internName; }
+
+    /**
+     * Return the age of this Intern
+     */
+    public int getInternAge() { return this.internAge; }
+
+    /**
+     * Return the skills of this Intern
+     */
+    public HashMap<String, Integer> getInternSkills() { return this.internSkills; }
+
+    /**
+     * Return a String of the Intern's given information.
+     */
+    public String internToString() {
+        StringBuilder information = new StringBuilder("Name: " + this.internName + "\n");
+        information.append("Age: ").append(this.internAge).append("\n");
+        information.append("Skills:\n");
+
+
+        for (String skill : this.internSkills.keySet()) {
+            int percentage = this.internSkills.get(skill);
+            information.append("-").append(skill).append(": ").append(percentage).append("\n");
+        }
+
+        return information.toString();
+    }
+}
