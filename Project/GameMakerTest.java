21862865297b545cd3c69890cd3091b3387c1228
camillealve <camillealve2014@gmail.com>
2021-10-14 20:32:33 -0400

    updated GameMakerTest

--- Project/GameMakerTest.java
+++ Project/GameMakerTest.java
@@ -13,9 +13,10 @@ class GameMakerTest {
     
     // Hi Camille, feel free to change this test as needed. - Maggie
     @Test(timeout = 50)
-    public void testFirstPrompt() {
-        String expectedResult = GamePrompts.FIRST_PROMPT_BEFORE_NAME + "Maggie" + "\n"
-            + GamePrompts.FIRST_PROMPT_AFTER_NAME
+    public void TestFirstPrompt() {
+        String expectedResult = "Welcome! My friend, you have just been hired as a new manager.\n"
+                + "Maggie" + "\n"
+            + "Here are your potential interns. Choose wisely ...\n"
             + "Name: Farzana Rahman; age: 20; skills: teamwork (98)\n"
             + "Name: Maggie Huang; age: 20; skills: leadership (82)\n"
             + "Name: Mary Yijia Li; age: 19; skills: efficiency (99)\n";
