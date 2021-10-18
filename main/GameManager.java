3d5aa2527b7b8ca480fbf5b310130b0de1a75636
huan2445 <maggiehuang1004@gmail.com>
2021-10-18 11:37:00 -0400

    Just some minor changes

--- main/GameManager.java
+++ main/GameManager.java
@@ -1,6 +1,6 @@
 public class GameManager {
-    /* this class is the controller class so it starts the game , waits for input etc for reference check JShell and
-    JShellState in week 2 resources on quercus
+    /* this class is the controller class so it starts the game , waits for input etc
+    for reference check JShell and JShellState in week 2 resources on quercus
     //TODO:
     - we need 1 method that gives us the output in the beginning of the game
     - need method that takes in a name and outputs the bigger prompt
