019366a31c59116017ba3ab9baf5438056684e30
frzana1012 <frahman.rahman@mail.utoronto.ca>
2021-10-22 17:27:31 -0400

    created classes for Employee (abstract), HiredIntern and InterviewMaker and wrote the TODOs with Camille and Enam. Will start implementing on monday.

---
+++ main/Employee.java
@@ -0,0 +1,19 @@
+abstract class Employee {
+    /*
+    this class will have everything intern has
+    - regular interns are part of the interviewee list
+    - hired intern will be able to take part in projects, have update_skill method and can get fired
+
+     TODO:
+     - implement hiredIntern
+     - implement interviewInterns
+
+     - Have methods:
+        - getInternName
+        - getInternAge
+        - getInternSkills
+        - internToString
+     */
+
+
+}
