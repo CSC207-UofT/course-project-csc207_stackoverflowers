e6870c6d027ea36b3842fc9de6afae0946fd9058
huan2445 <maggiehuang1004@gmail.com>
2021-10-26 16:51:43 -0400

    Worked on the reports

--- main/Report/ReportPresenter.java
+++ main/Report/ReportPresenter.java
@@ -1,7 +1,21 @@
 package Report;
 
-public class ReportPresenter {
-    //an abstract class
+import java.util.List;
+
+//an abstract class
+public class ReportPresenter implements ReportMaker {
     //TODO: instantiate
+    public ReportPresenter () {}
+
     //TODO: void methods
+    void displayReport (){
+        String header = makeReportHeader();
+        String intro = makeReportIntro();
+        String body = makeReportBody();
+        String end = endReport();
+        System.out.println(header + "\n" +
+                "Project Intro:" + intro + "\n" +
+                body + "\n" +
+                end);
+    }
 }
