15c11466b86ea0d4b3bd367204fd622987ef7eb2
Jacobcansleepnow <jizhong50@gmail.com>
2021-10-26 19:48:16 -0400

    first draft on TODOs in ReportMaker.java

--- main/Report/ReportMaker.java
+++ main/Report/ReportMaker.java
@@ -1,31 +1,29 @@
 package Report;
 
 public class ReportMaker {
-    // An abstract class, that have all the essentials needed to make a report.
-    //TODO: instantiate
-    //TODO: * an interface class that shows all common methods used to make reports:
-    //TODO: * makeReportHeader()
-    //TODO:  * makeReportIntro()
-    //TODO:  * makeReportBody()
-    //TODO:  * endReport()
+    //An interface class that shows all common methods used to make reports:
+
+    //TODO: makeReportHeader(),takes in a String "timeName"
+    //TODO: Change raw Strings to reference to gamePrompts
     private String makeReportHeader (String timeName){
         return "Here is your report for the end of " + timeName + ": ";
     }
 
+    //TODO: makeReportIntro(), takes in information related to intro
+    //TODO: Change raw Strings to reference to gamePrompts
     private String makeReportIntro (){
         return "";
     }
 
+    //TODO: makeReportBody(), takes in information related to body
+    //TODO: Change raw Strings to reference to gamePrompts
     private String makeReportBody (){
         return "";
     }
 
+    //TODO: endReport()
+    //TODO: Change raw Strings to reference to gamePrompts
     private String endReport(){
         return "That's all! Have a good day manager. :)";
     }
-    //TODO: void methods:
-    //makeReportHeader()
-    //makeReportIntro()
-    //makeReportBody()
-    //endReport()
 }
