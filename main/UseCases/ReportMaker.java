2691b809f0e15a86b7dc75b0f6899aae8b15117c
Jacobcansleepnow <jizhong50@gmail.com>
2021-10-28 22:34:37 -0400

    deleted extra comments that are out of date since we are using the new template for any types of report. Plus, this is just an interface, Mary told me that interface can't have method body.

--- main/UseCases/ReportMaker.java
+++ main/UseCases/ReportMaker.java
@@ -8,18 +8,18 @@ package UseCases;
 public interface ReportMaker {
 
     String makeReportHeader (int month);
-        //return "Here is your report for the end of " + month;
+
 
 
     String makeReportIntro ();
-        //return "";
+
 
 
     String makeReportBody ();
-        //return "";
+
 
 
     String makeReportConclusion();
-        //return REPORT_CONCLUSION;
+
 
 }
