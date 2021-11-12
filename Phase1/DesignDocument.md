02934d45e4578fd8ed93d54672c50518f347b093
hermonen <enam.hermon@mail.utoronto.ca>
2021-11-12 08:38:46 -0500

    HRSystem updates, Report makers updated, interview makers updated

--- Phase1/DesignDocument.md
+++ Phase1/DesignDocument.md
@@ -88,6 +88,16 @@ a separate factory class ReportFactory that is responsible for only evaluating p
 - We also kind of got inspiration from the Observer Design pattern, as our Abstract Class Level will change statuses, and those statuses need to be reported to GameManager. Right now we are making GameManager check each time if the status of the currentLevel has changed, so that it fits more into the Observer design pattern. 
 ADD TO THIS!!!!
 
+4. TEMPLATE DESIGN PATTERN 
+
+We incorporated a template design pattern when creating an abstraction for our Entities.Intern class which was formerly a single class. 
+With our current project, we initially created an entity for an Intern (which is a character in our HR System Simulator that is assigned projects after being interviewed). 
+Upon design of the code, we realized there is a need for distinction between two types of Intern, i.e. a HiredIntern (an intern who has passed the interview process and can be assigned projects) and an InterviewIntern (which engages in the Interview Phase of the project). 
+Thus, Intern was abstracted, implementing the invariant methods, leaving variant methods to be implemented by the unique characteristics of the subclasses HiredIntern and InterviewIntern. 
+In this way, Intern is our template that can be extended in the instance where a new type of Intern needs to be defined with its own unique behaviours.
+This was implemented after completion of Phase 0, during planning for new classes to be implemented in Phase 1.
+
+
 
 Have you clearly indicated where the pattern was used and possibly pointed out which Pull Request it was implemented in?
 Be careful that there aren't any obvious places a design pattern should have been applied that your group forgot to mention.
--- main/UseCases/FinalReportMaker.java
+++ main/UseCases/FinalReportMaker.java
@@ -6,6 +6,7 @@ import Entities.Intern;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+@SuppressWarnings("DuplicatedCode")
 public class FinalReportMaker implements ReportMaker {
     private final GamePrompts prompts;
     private final HRSystem currentHRSystem;
@@ -29,15 +30,16 @@ public class FinalReportMaker implements ReportMaker {
         for (Project proj : projList) {
             projectCompatibilityList.putAll(proj.getSkillsCompatibilities());
         }
-        ArrayList<HashMap<String, Integer>> internsSkills = getHiredInternsSkills(currentHRSystem.getHiredInternList());
+        ArrayList<HashMap<String, Double>> internsSkills = getHiredInternsSkills(currentHRSystem.getHiredInternList());
         return bakeProjectName(currentHRSystem.getProjectName(currentMonth)) + "\n" +
                 bakeProgress(projectProgress)+"\n"+
                 bakeInterns(internNames) + "\n" +
                 bakeInternsPerformances(internNames, internsSkills, projectCompatibilityList);
     }
-    private ArrayList<HashMap<String, Integer>> getHiredInternsSkills(ArrayList<HiredIntern> hiredInternList) {
+
+    private ArrayList<HashMap<String, Double>> getHiredInternsSkills(ArrayList<HiredIntern> hiredInternList) {
         //Makes an arrayList full of internSkills.
-        ArrayList<HashMap<String, Integer>> internCompatabilityList  = new ArrayList<>();
+        ArrayList<HashMap<String, Double>> internCompatabilityList  = new ArrayList<>();
         for (Intern i : hiredInternList){
             internCompatabilityList.add(i.getInternSkills());
         }
@@ -59,7 +61,7 @@ public class FinalReportMaker implements ReportMaker {
     }
 
     @Override
-    public String bakeInternsPerformances (String internNames, ArrayList<HashMap<String, Integer>>  internSkills, HashMap<String, Integer> projectSkill) {
+    public String bakeInternsPerformances (String internNames, ArrayList<HashMap<String, Double>>  internSkills, HashMap<String, Integer> projectSkill) {
         StringBuilder returnLine = new StringBuilder(GamePrompts.INTERN_PERFORMANCE_HEADER + internNames + "\n");
         String[] internNamesList = internNames.split("|");
         for (int i = 0; i != internNamesList.length; i+=1) {
@@ -69,11 +71,11 @@ public class FinalReportMaker implements ReportMaker {
     }
 
     @Override
-    public int calculateInternPerformance(HashMap<String, Integer> internSkills, HashMap<String, Integer> projectSkill) {
+    public int calculateInternPerformance(HashMap<String, Double> internSkills, HashMap<String, Integer> projectSkill) {
         int result = 0;
         ArrayList<Double> effectiveSkills = new ArrayList<Double>();
         for (String key : internSkills.keySet()) {
-            int internSkill = internSkills.get(key);
+            double internSkill = internSkills.get(key);
             double compatibility = projectSkill.get(key);
             effectiveSkills.add(internSkill* compatibility);
         }
