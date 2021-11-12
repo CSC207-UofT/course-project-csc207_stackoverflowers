d354b89851e404a7ec7569c110aa60adb1a8ebf8
huan2445 <maggiehuang1004@gmail.com>
2021-11-11 19:30:36 -0500

    Wrote tests for Game Generator

--- main/UseCases/GameGenerators.java
+++ main/UseCases/GameGenerators.java
@@ -10,18 +10,14 @@ import java.io.*;
 import java.util.*;
 
 public class GameGenerators implements Serializable {
-    private final GamePrompts prompts;
     private final HRSystem currentHRSystem;
 
-
     public GameGenerators(){
-        this.prompts = new GamePrompts();
         this.currentHRSystem = new HRSystem();
     }
 
     /**
      * Add the list of interns to UseCases.HRSystem.
-     *
      * @param newInterns A list of interns constructed in the UseCases.GameMaker constructor
      */
     public void addInternToList(ArrayList<Intern> newInterns){
@@ -29,9 +25,8 @@ public class GameGenerators implements Serializable {
     }
 
     /**
-     * Generates an ArrayList of new random interns/interviewees.
+     * Randomly generates an ArrayList of interns/interviewees, and adds it to UseCases.HRSystem
      * @param numInterns the number of interns that will be generated.
-     * @return an ArrayList of randomly generated Interns.
      */
     public void generateInterns(int numInterns) throws FileNotFoundException {
         ArrayList<String> nameList = generateInfo("UseCases/names.txt");
@@ -41,7 +36,7 @@ public class GameGenerators implements Serializable {
         for (int i = 1; i <= numInterns; i++) {
             String name = nameList.get(random.nextInt(nameList.size()));
             nameList.remove(name);
-            HashMap<String, Integer> skillMap = generateUniqueSkillMap(skillList);
+            HashMap<String, Double> skillMap = generateUniqueSkillMap(skillList);
             int age = random.nextInt(45) + 20;
             Intern interviewee = new InterviewIntern(name, age, skillMap);
             internList.add(interviewee);
@@ -50,11 +45,9 @@ public class GameGenerators implements Serializable {
     }
 
     /**
-     * Generates a random list of projects that player will be interacting with during the game.
-     * Creates an ArrayList of Projects.
-     * updates current HR system to reflect the projects that will be used for this game.
+     * Randomly generates an ArrayList of projects that player will be interacting with during the game,
+     * and updates the current HR system to reflect the projects that will be used for this game.
      * @param proj the number of projects that will be generated.
-     * @return an ArrayList of projects.
      */
     public void generateProjects(int proj) throws FileNotFoundException {
         // take the project prompts from GamePrompts and outputs a list
@@ -77,10 +70,10 @@ public class GameGenerators implements Serializable {
         }
         currentHRSystem.updateProjectList(projForGame);
     }
+
     /**
-     * Generates a random final project that player will be interacting with during the game.
-     * updates current HR system to reflect the final project that will be used for this game.
-     * @return an ArrayList containing one final project.
+     * Generates an ArrayList containing one randomly generated final project that player will be interacting with
+     * during the game, and updates the current HR system to reflect the final project that will be used for this game.
      */
     public void generateFinalProject() throws FileNotFoundException {
         // take the final projects in gamePrompts and pick a final project for this game.
@@ -98,7 +91,7 @@ public class GameGenerators implements Serializable {
     }
 
     /**
-     * Helper function for generateInterns.
+     * Helper method for generateInterns.
      * Converts a text file into an ArrayList, line by line.
      * @param fileName the name of the file that is going to be converted
      * @return an ArrayList of Entities.Intern names or Entities.Intern skills
@@ -117,23 +110,17 @@ public class GameGenerators implements Serializable {
      * Helper method for generateInterns.
      * Creates a HashMap with three random skills and their random respective percentages
      * @param skillList an ArrayList of skills.
-     * @return a HashMap of Skills and the percentage of the skill
+     * @return a HashMap of Skills and the score (0.00 - 1.00) of the skill
      */
-    private HashMap<String, Integer> generateUniqueSkillMap(ArrayList<String> skillList){
+    private HashMap<String, Double> generateUniqueSkillMap(ArrayList<String> skillList){
         Random random = new Random();
-        HashMap<String, Integer> skillMap = new HashMap<>();
+        HashMap<String, Double> skillMap = new HashMap<>();
         while (skillMap.size() < 3) {
             String skill = skillList.get(random.nextInt(skillList.size()));
             if (!skillMap.containsKey(skill)) {
-                skillMap.put(skill, random.nextInt(101));
+                skillMap.put(skill, 2.00 * random.nextDouble());
             }
         }
         return skillMap;
     }
-
-
-
-
-
-
 }
