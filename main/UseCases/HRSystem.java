281c53455b04fbb7df3352e16e2ffba5c6dbcca4
frzana1012 <frahman.rahman@mail.utoronto.ca>
2021-11-12 14:11:56 -0500

    Changes by Mary

--- main/UseCases/HRSystem.java
+++ main/UseCases/HRSystem.java
@@ -386,7 +386,7 @@ public class HRSystem implements Serializable {
         return options;
 
     }
-
+    
     public void updatePlayerInternResponseChoice(Object option){
         this.playerInternResponseChoice = option;
     }
