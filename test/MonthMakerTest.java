db9467333fe6cce49c88a651b1b6980c2b58415f
Mary Yijia Li <maryyijia.li@mail.utoronto.ca>
2021-11-16 22:16:40 -0500

    Modified strings to make game more readable.

--- test/MonthMakerTest.java
+++ test/MonthMakerTest.java
@@ -29,7 +29,7 @@ public class MonthMakerTest {
         monthMaker = new MonthMaker(hrSystem, 5);
         String actual = monthMaker.startOfMonthPrompt();
         String expected = GamePrompts.FINAL_MONTH_PROMPT_BEFORE_PROJECT + monthMaker.getProjectInfo() +
-                GamePrompts.FINAL_MONTH_PROMPT_AFTER_PROJECT;
+                GamePrompts.AVAILABLE_COMMANDS_IN_MONTH;
         assertEquals(expected, actual);
     }
 
