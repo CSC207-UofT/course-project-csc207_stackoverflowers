a30a94580ace3c815d994d5c938b5d1a64c05ab2
Maryyeah <maryyijia.li@mail.utoronto.ca>
2021-12-04 09:11:42 -0500

    Added more tests for GameManagerTest and GameMakerTest.

--- test/GameMakerTest.java
+++ test/GameMakerTest.java
@@ -13,16 +13,17 @@ import java.io.*;
 public class GameMakerTest {
     GameMaker gameMaker;
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
          gameMaker = new GameMaker();
          gameMaker.setCurrentMonth(3);
+         gameMaker.firstPrompt("Maggie"); //initialize the game so that it can be serialized tested
     }
     
     // Hi Camille, feel free to change this test as needed. - Maggie
     @Test
     public void TestFirstPrompt() throws Exception {
         String expectedResult = GamePrompts.FIRST_PROMPT_BEFORE_NAME + "Maggie" + GamePrompts.FIRST_PROMPT_AFTER_NAME +
-                gameMaker.getCurrentHRSystem().makeInterviewInternsToString() + GamePrompts.START_INTERVIEW_PROMPT;
+                gameMaker.getCurrentHRSystem().makeInterviewInternsToString() + GamePrompts.FIRST_PROMPT_AFTER_INTERNS;
         assertEquals(expectedResult, gameMaker.firstPrompt("Maggie"));
     }
 
@@ -49,7 +50,7 @@ public class GameMakerTest {
     @Test
     public  void TestLoad() throws IOException, ClassNotFoundException {
         gameMaker.save(1, "FinalMonth");
-        GameMaker copy = gameMaker.load(gameMaker.getCurrentHRSystem().getPlayerName());
+        GameMaker copy = gameMaker.load(gameMaker.getCurrentHRSystem().getPlayerName()+"_0");
         assertEquals(gameMaker.getCurrentHRSystem().getPlayerName(), copy.getCurrentHRSystem().getPlayerName());
         assertEquals(gameMaker.getCurrentHRSystem().getInterviewInternList(), copy.getCurrentHRSystem().getInterviewInternList());
     }
--- test/GameManagerTest.java
+++ test/GameManagerTest.java
@@ -5,6 +5,9 @@ import Entities.Exceptions;
 import Entities.GamePrompts;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -15,23 +18,24 @@ public class GameManagerTest {
     public void setUp() throws Exception {
         gameManager = new GameManager();
 
+
+    }
+    @Test
+    public void testLoad() throws Exception{
+        gameManager.getOutput("Maggie");
+        try{gameManager.getOutput("load haha");}
+        catch (Exception e) {
+            assertEquals("Can't find file to load",e.getMessage());
+        }
+    }
+    @Test
+    public void testLoadSuccess() throws Exception {
+        gameManager.getOutput("load Maggie_0");
     }
 
     @Test
-    public void testLoad() throws Exception{
-        gameManager.getOutput("load haha");
+    public void testGetOutput() throws Exception {
+        String result = gameManager.getOutput("quit");
+        String expected = GamePrompts.INFORM_QUIT_GAME + "Maggie";
     }
-
-//    @Test
-//    public void testStartToInterview(){
-//        assertTrue(gameManager.currentLevel instanceof InterviewLevel);
-//    }
-
-//    @Test
-//    public void testInterviewToMonth() throws Exception {
-//        gameManager.currentLevel.endLevel();
-//        gameManager.getOutput("ha");
-//        gameManager.getOutput("now");
-//        assertTrue(gameManager.currentLevel instanceof MonthLevel);
-//    }
 }
