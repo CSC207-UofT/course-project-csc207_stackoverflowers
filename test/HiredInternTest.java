ca89de0aff71fccacb6a39a62fc59fc7e4a14d7b
Maryyeah <maryyijia.li@mail.utoronto.ca>
2021-12-05 23:42:34 -0500

    wrote test

---
+++ test/HiredInternTest.java
@@ -0,0 +1,17 @@
+import Entities.HiredIntern;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class HiredInternTest {
+    @Test
+    public void testHiredIntern (){
+        HashMap<String, Double> internSkills = new HashMap<>();
+        internSkills.put("Smart", 10.0);
+        HiredIntern hiredIntern = new HiredIntern("Mar", 21, internSkills);
+        hiredIntern.updateInternSkills("hi");
+        assertEquals(java.util.Optional.ofNullable(hiredIntern.getInternSkills().get("hi")), 5);
+    }
+}
