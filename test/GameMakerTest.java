7201a8f7c2c6c94de1b59633c71485f2491b9920
Maryyeah <maryyijia.li@mail.utoronto.ca>
2021-12-07 16:42:22 -0500

    Updated so that gameManager and gameMaker tests pass.

--- test/GameMakerTest.java
+++ test/GameMakerTest.java
@@ -13,16 +13,17 @@ import java.io.*;
 public class GameMakerTest {
     GameMaker gameMaker;
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
          gameMaker = new GameMaker();
          gameMaker.setCurrentMonth(3);
+         gameMaker.getCurrentHRSystem().updatePlayerName("Maggie");
     }
     
     // Hi Camille, feel free to change this test as needed. - Maggie
     @Test
     public void TestFirstPrompt() throws Exception {
         String expectedResult = GamePrompts.FIRST_PROMPT_BEFORE_NAME + "Maggie" + GamePrompts.FIRST_PROMPT_AFTER_NAME +
-                gameMaker.getCurrentHRSystem().makeInterviewInternsToString() + GamePrompts.START_INTERVIEW_PROMPT;
+                gameMaker.getCurrentHRSystem().makeInterviewInternsToString() + GamePrompts.FIRST_PROMPT_AFTER_INTERNS;
         assertEquals(expectedResult, gameMaker.firstPrompt("Maggie"));
     }
 
@@ -32,14 +33,10 @@ public class GameMakerTest {
         assertEquals(expectedResult, gameMaker.endPrompt());
     }
 
-//    @Test(timeout = 1000)
-//    public void TestGenerateInterns() throws FileNotFoundException {
-//        gameMaker.generateInterns(3);
-//    }
 
     @Test
     public void TestSerializable() throws IOException {
-            new ObjectOutputStream(new FileOutputStream("Maggie")).writeObject(gameMaker);
+        new ObjectOutputStream(new FileOutputStream("Maggie")).writeObject(gameMaker);
 
     }
     @Test
@@ -49,7 +46,7 @@ public class GameMakerTest {
     @Test
     public  void TestLoad() throws IOException, ClassNotFoundException {
         gameMaker.save(1, "FinalMonth");
-        GameMaker copy = gameMaker.load(gameMaker.getCurrentHRSystem().getPlayerName());
+        GameMaker copy = gameMaker.load(gameMaker.getCurrentHRSystem().getPlayerName()+ "_0");
         assertEquals(gameMaker.getCurrentHRSystem().getPlayerName(), copy.getCurrentHRSystem().getPlayerName());
         assertEquals(gameMaker.getCurrentHRSystem().getInterviewInternList(), copy.getCurrentHRSystem().getInterviewInternList());
     }
